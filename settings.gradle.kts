261d82579bfc151c5218d1404e46a53cc0229109
Coen Hallie <coenhallie@gmail.com>
2026-05-04 15:14:04 +0200

    chore: gradle root setup

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "badminton-rally-mobile"
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":shared")
